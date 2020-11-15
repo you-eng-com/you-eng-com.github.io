44f746adfdb35a8958dd0091f964c36cbf794b93
Alex <anebotov@gmail.com>
2020-11-15 23:36:36 +0300

    v0.0.0

---
+++ precache-manifest.658991f6e4a5e933de3f747301545e3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7269f160784417c887eb5296c45325c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78f7d8d0108c154610e0",
+    "url": "/static/css/main.48a147d8.chunk.css"
+  },
+  {
+    "revision": "c4d3a8bdd63981cd05ba",
+    "url": "/static/js/2.83bb1ddc.chunk.js"
+  },
+  {
+    "revision": "0445d5183d0141ac77c6557b72598011",
+    "url": "/static/js/2.83bb1ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78f7d8d0108c154610e0",
+    "url": "/static/js/main.1a6d1946.chunk.js"
+  },
+  {
+    "revision": "988ce9d892edebd9c71e",
+    "url": "/static/js/runtime-main.e3811507.js"
+  }
+]);
